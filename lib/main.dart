bb565c11e0a657558b47feac66868d2308219ed6
KiranJoshi-Flutter <KiranJoshi-Flutter@users.noreply.github.com>
2021-07-20 17:59:17 +0545

    Container, Container() inside Container(), color, padding

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Container(
-        child: Text('Kiran Joshi'),
+        color: Colors.blue,
+        padding: EdgeInsets.only(
+          left: 20.0,
+          top: 50.0,
+          right: 250.0,
+          bottom: 700.0,
+        ),
+        child: Container(
+          color: Colors.green,
+        ),
       ),
     );
   }
